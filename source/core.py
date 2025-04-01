5bfa480177548278c288e769ec47a02fa481febd
toka <nam.hatankhanh@gmail.com>
2025-04-01 14:41:09 +0700

    add news analyzer wrapper

---
+++ source/core.py
@@ -0,0 +1,75 @@
+import json
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chat_models import init_chat_model
+
+# ==
+class NewsAnalysis():
+    def __init__(self, backbone_model, backbone_model_provider):
+        self._config_file = "configs/core.json"
+        with open(self._config_file, "r") as file:
+            self._config = json.load(file)
+            
+        self._llm = init_chat_model(
+            model = backbone_model, 
+            model_provider = backbone_model_provider
+        )
+        
+    def summarize(self, text):            
+        system_template = self._config['summarization']['system_template']
+        prompt_template = ChatPromptTemplate.from_messages(
+            [("system", system_template), ("user", "{text}")]
+        )
+        
+        prompt = prompt_template.invoke(
+            {
+                "text": text
+            }
+        )
+        
+        response = self._llm.invoke(prompt).content
+        
+        return response
+
+    def categorize(self, text):
+        system_template = self._config['categorization']['system_template']
+        prompt_template = ChatPromptTemplate.from_messages(
+            [("system", system_template), ("user", "{text}")]
+        )
+        
+        prompt = prompt_template.invoke(
+            {
+                "categories": self._config['categorization']['categories'],
+                "text": text
+            }
+        )
+        
+        response = self._llm.invoke(prompt).content
+        
+        return response
+    
+    def extract_keywords(self, text):
+        system_template = self._config['keywords_extraction']['system_template']
+        prompt_template = ChatPromptTemplate.from_messages(
+            [("system", system_template), ("user", "{text}")]
+        )
+        
+        prompt = prompt_template.invoke(
+            {
+                "text": text
+            }
+        )
+        
+        response = self._llm.invoke(prompt).content
+        
+        try:
+            response = json.loads(response)
+            
+        except Exception as e:
+            #TODO: Handle retry
+            pass
+        
+        return response
+    
+    def process(self, text):
+        pass
---
+++ test/test_core.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from source.core import NewsAnalysis
+
+# ==
+def main():
+    analyzer = NewsAnalysis(
+        backbone_model = "gpt-4o-mini",
+        backbone_model_provider = "openai"
+    )
+    
+    with open("dataset/test_news.txt", "r") as file:
+        news_content = file.read()
+            
+    summary = analyzer.summarize(news_content)
+    category = analyzer.categorize(news_content)
+    keywords = analyzer.extract_keywords(news_content)
+    
+    print(f"==\n{news_content}\n==\nSummary: {summary}\nCategory: {category}\nKeywords: {keywords}\n==")
+    
+    
+if __name__ == "__main__":
+    main()
